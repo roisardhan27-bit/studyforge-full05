fec6237f18e5017c1c871cd47ed08cd349ac7bba
roisardhan27-bit <rois.ardhan27@smp.belajar.id>
2026-03-13 17:34:31 +0700

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="bg-blue-600 text-white p-4 rounded-b-lg shadow-md text-center">
+      <h1 className="text-xl font-bold">StudyForge</h1>
+      <p className="text-sm">Build Your Best Self</p>
+    </header>
+  );
+}
